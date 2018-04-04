70caf88c0cfdf3a6341ad9e844621b86ab6d29a5
Chris Manning <cmanning@vt.edu>
2018-04-03 23:00:00 -0400

    RMSE & Jacobian

--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,66 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+
+  // initialize variables
+	VectorXd rmse(4);
+	rmse << 0,0,0,0;
+
+	// check the validity of the following inputs:
+	//  * the estimation vector size should not be zero
+	if ( estimations.size() == 0 ) {
+        cout << "CalculateRMSE () - Error - Estimations vector size must be nonzero" << endl;
+        return rmse;
+    }
+	//  * the estimation vector size should equal ground truth vector size
+	if ( estimations.size() != ground_truth.size() ) {
+        cout << "CalculateRMSE () - Error - Input vectors must have same size" << endl;
+        return rmse;
+    }
+
+	//accumulate squared residuals
+	for(int i=0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i] - ground_truth[i];
+        residual = residual.array()*residual.array();
+		rmse += residual;
+	}
+
+	//calculate the mean
+	rmse = rmse / estimations.size();
+
+	//calculate the squared root
+	rmse = rmse.array().sqrt();
+
+	//return the result
+	return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+
+  // initialize variables
+	MatrixXd Hj(3,4);
+  
+	//recover state parameters
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//check division by zero
+	if ( px == 0 && py == 0 ) {
+        cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+        return Hj;
+    }
+	
+	//compute the Jacobian matrix
+    Hj(0, 0) =  px                     / sqrt( px*px + py*py );
+    Hj(0, 1) =  py                     / sqrt( px*px + py*py );
+    Hj(1, 0) = -py                     /     ( px*px + py*py );
+    Hj(1, 1) =  px                     /     ( px*px + py*py );
+    Hj(2, 0) =  py * ( vx*py - vy*px ) /  pow( px*px + py*py , 3/2 );
+    Hj(2, 1) =  px * ( vy*px - vx*py ) /  pow( px*px + py*py , 3/2 );
+    Hj(2, 2) =  px                     / sqrt( px*px + py*py );
+    Hj(2, 3) =  py                     / sqrt( px*px + py*py );
+
+	return Hj;
 }
