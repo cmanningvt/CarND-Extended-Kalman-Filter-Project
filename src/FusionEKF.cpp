960f0c9bbcab0bb64393f47f71643d9b86d2e251
Chris Manning <cmanning@vt.edu>
2018-04-12 19:43:23 -0400

    AdditionalDebug

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -141,7 +141,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   ekf_.Predict();
 
-  cout << "x_predict = " << ekf_.x_ << endl;
+  //cout << "x_predict = " << ekf_.x_ << endl;
 
   /*****************************************************************************
    *  Update
@@ -158,6 +158,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.R_ = R_radar_;
     ekf_.H_ = Hj_;
 
+    cout << "R_radar_ = " << ekf_.R_ << endl;
+    cout << "Hj_      = " << ekf_.H_ << endl;
+
     ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
@@ -168,6 +171,6 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
   }
 
   // print the output
-  cout << "x_update = " << ekf_.x_ << endl;
+  //cout << "x_update = " << ekf_.x_ << endl;
   //cout << "P_ = " << ekf_.P_ << endl;
 }
