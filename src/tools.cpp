3534906701057b00b8f04c7d5ea61730f0c1b08d
Chris Manning <cmanning@vt.edu>
2018-04-14 16:16:01 -0400

    test

--- src/tools.cpp
+++ src/tools.cpp
@@ -44,7 +44,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 	//return the result
 	return rmse;
 }
-
+/*
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 
   // initialize variables
@@ -75,6 +75,39 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
     Hj(2, 1) =  px * ( vy*px - vx*py ) /  pow( px*px + py*py , 3/2 );
     Hj(2, 2) =  px                     / sqrt( px*px + py*py );
     Hj(2, 3) =  py                     / sqrt( px*px + py*py );
+
+	return Hj;
+}
+*/
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+  TODO:
+    * Calculate a Jacobian here.
+  */
+	// Initiate Jacobian Matrix
+	MatrixXd Hj(3, 4);
+
+	float px = x_state(0);
+	float py = x_state(1);
+	float vx = x_state(2);
+	float vy = x_state(3);
+
+	//pre-compute a set of terms to avoid repeated calculation
+	float c1 = px*px + py*py;
+	float c2 = sqrt(c1);
+	float c3 = (c1*c2);
+
+	//check division by zero
+	if (fabs(c1) < 0.0001) {
+		cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+		return Hj;
+	}
+
+	//compute the Jacobian matrix
+	Hj << (px / c2), (py / c2), 0, 0,
+		-(py / c1), (px / c1), 0, 0,
+		py*(vx*py - vy*px) / c3, px*(px*vy - py*vx) / c3, px / c2, py / c2;
 
 	return Hj;
 }
